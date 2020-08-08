0780e35bb470d39ed96dc49c8aed74e56ca95f8a
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-08-08 13:55:32 -0400

    Basic tetris game

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import Tetris from './components/Tetris'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Tetris boardWidth="14" boardHeight="20" />,
   document.getElementById('root')
 );
 
